f2c4d6a3e712193350ebe79dc01c3fe46157b940
thenamelessdev <nameless@thenamelessdev.com>
2026-04-26 18:42:17 +0200

    upate: prisma schema

--- prisma/migrations/20260426164150_init/migration.sql
+++ prisma/migrations/20260426164150_init/migration.sql
@@ -3,7 +3,16 @@ CREATE TABLE "Users" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+    "verifedEmail" BOOLEAN NOT NULL DEFAULT false,
+    "emailVerifyCode" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Pages" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "ownerId" INTEGER NOT NULL
 );
 
 -- CreateIndex
