aec1e42753943defdf6ef8947644ccc0386e86a6
thenamelessdev <nameless@thenamelessdev.com>
2026-04-26 19:16:21 +0200

    feat: add createSiteForm function

--- lib/sites/createSite.ts
+++ lib/sites/createSite.ts
@@ -3,6 +3,8 @@
 import path from "path";
 import { db } from "../db";
 import fs from "fs/promises";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function createSite(ownerId: number, title: string, html: Buffer) {
     const htmlStr = html.toString("utf8")
@@ -19,4 +21,23 @@ export async function createSite(ownerId: number, title: string, html: Buffer) {
     await fs.mkdir(pagePath, {recursive: true});
     const htmlPath = path.join(pagePath, "page.html");
     await fs.writeFile(htmlPath, html)
+}
+
+export async function createSiteForm(formData: FormData) {
+    const title = formData.get("title") as null | string;
+    const htmlFile = formData.get("html") as null | File;
+
+    if(!title || !htmlFile) return;
+
+    const htmlBuffer = Buffer.from(await htmlFile.arrayBuffer())
+    
+    const username = (await headers()).get("x-username");
+    if(!username) return redirect("/login");
+    const user = await db.users.findUnique({
+        where: {
+            username: username
+        }
+    })
+
+    await createSite(user?.id!, title, htmlBuffer);
 }
