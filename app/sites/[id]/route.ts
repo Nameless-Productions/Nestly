e32226f009df77938695f97815094a74abaf5e31
thenamelessdev <nameless@thenamelessdev.com>
2026-04-26 22:19:32 +0200

    feat: add ability to view pages

---
+++ app/sites/[id]/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+import { getSite } from "@/lib/sites/getSite";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: { id: number }}) {
+    const {id} = await params;
+    const page = await db.pages.findUnique({
+        where: {
+            id: Number(id)
+        }
+    });
+    if(!page) return NextResponse.redirect(new URL("/sites", req.url), 302);
+
+    const pageHtml = await getSite(page.id);
+    if(!pageHtml) return NextResponse.redirect(new URL("/sites", req.url), 302);
+
+    return new Response(pageHtml, {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    })
+}
